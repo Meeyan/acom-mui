d982641c741866d422d1001c89b6afd429912ad0
Meeyan <victoryai@126.com>
2016-08-31 00:54:27 +0800

    系统代码初始化完善中

---
+++ src/main/src/com/acom/Test.java
@@ -0,0 +1,10 @@
+package com.acom;
+
+/**
+ * Created by Jiay on 2016/8/30.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hhhhahah");
+    }
+}
