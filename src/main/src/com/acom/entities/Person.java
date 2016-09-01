aadbf7810f3e1bf759e610659d41d23329728684
Meeyan <victoryai@126.com>
2016-09-01 23:37:05 +0800

    #2016-09-01 bean创建

--- src/main/src/com/acom/entities/Person.java
+++ src/main/src/com/acom/entities/Person.java
@@ -4,4 +4,32 @@ package com.acom.entities;
  * Created by Jiay on 2016/8/31.
  */
 public class Person {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+
+    private String name;
+    private int sex;
+    private String addr;
+
 }
