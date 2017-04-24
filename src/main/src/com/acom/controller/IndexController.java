f7f63397a84fc55cf58237aaeac10477dbbce4c5
zhaojy <victoryai@126.com>
2017-04-24 16:42:26 +0800

    #2017-04-24 完善spring配置

--- src/main/src/com/acom/controller/IndexController.java
+++ src/main/src/com/acom/controller/IndexController.java
@@ -1,11 +1,27 @@
 package com.acom.controller;
 
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
 /**
- * Created by Jiay on 2016/8/31.
+ * 首页
  */
+@Controller
+@RequestMapping(value = "")
 public class IndexController {
+
+    private Logger log = Logger.getLogger(IndexController.class);
 
     public Object index() {
         return null;
+    }
+
+    @RequestMapping(value = "/test")
+    public ModelAndView test() {
+        ModelAndView view = new ModelAndView();
+        view.setViewName("index");
+        return view;
     }
 }
