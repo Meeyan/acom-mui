d0e2dcaa03ccccb174772f229b08a58eabd2b92b
zhaojy <victoryai@126.com>
2017-04-24 17:07:37 +0800

    #2017-04-24 测试

--- src/main/src/com/acom/controller/TestController.java
+++ src/main/src/com/acom/controller/TestController.java
@@ -9,10 +9,10 @@ import org.springframework.web.servlet.ModelAndView;
  * 首页
  */
 @Controller
-@RequestMapping(value = "")
-public class IndexController {
+@RequestMapping(value = "/test")
+public class TestController {
 
-    private Logger log = Logger.getLogger(IndexController.class);
+    private Logger log = Logger.getLogger(TestController.class);
 
     /**
      * 网站默认映射到该路径下，webapp下不能有index.jsp文件，或者在index.jsp中导向到'/'
@@ -23,6 +23,13 @@ public class IndexController {
     public ModelAndView index() {
         ModelAndView view = new ModelAndView();
         view.setViewName("index");
+        return view;
+    }
+
+    @RequestMapping(value = "/test")
+    public ModelAndView test() {
+        ModelAndView view = new ModelAndView();
+        view.setViewName("test");
         return view;
     }
 }
